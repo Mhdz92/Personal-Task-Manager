94b0b0ac11bdd7ae73d2e1509b624dc8c87ea2f5
mehdiz <m.zahed929@gmail.com>
2025-07-14 21:32:11 +0200

    some endpoints

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+@app.get('/PTM')
+async def get_all_tasks():
+    return {"message": "tasklist"}
+
+@app.post("/PTM/{create_task}")
+async def create_task(create_task: str):
+    tasklist = []
+    tasklist.append(create_task)
+    return tasklist
+
+@app.put("/PTM/task_id")
+async def update_task(tasklist: str, query: str):
+
+
+
+
+
+
